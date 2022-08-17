b648968a58f49af7b8d031744c09d55ff86682e4
Cesar de la Vega <cesarvegaro@gmail.com>
2022-08-17 14:42:33 +0200

    Prevents edit_changelog and branch confirmation in CI (#21)

--- lib/fastlane/plugin/revenuecat_internal/actions/bump_version_update_changelog_create_pr_action.rb
+++ lib/fastlane/plugin/revenuecat_internal/actions/bump_version_update_changelog_create_pr_action.rb
@@ -20,7 +20,7 @@ module Fastlane
         editor = params[:editor]
 
         current_branch = Actions.git_branch
-        unless UI.confirm("Current branch is #{current_branch}. Are you sure this is the base branch for your bump?")
+        if UI.interactive? && !UI.confirm("Current branch is #{current_branch}. Are you sure this is the base branch for your bump?")
           UI.user_error!("Cancelled during branch confirmation")
         end
 
@@ -36,7 +36,11 @@ module Fastlane
         Helper::RevenuecatInternalHelper.validate_local_config_status_for_bump(new_branch_name, github_pr_token)
 
         generated_contents = Helper::VersioningHelper.auto_generate_changelog(repo_name, github_token, rate_limit_sleep)
-        Helper::RevenuecatInternalHelper.edit_changelog(generated_contents, changelog_latest_path, editor)
+
+        if UI.interactive?
+          Helper::RevenuecatInternalHelper.edit_changelog(generated_contents, changelog_latest_path, editor)
+        end
+
         changelog = File.read(changelog_latest_path)
 
         Helper::RevenuecatInternalHelper.create_new_branch_and_checkout(new_branch_name)
