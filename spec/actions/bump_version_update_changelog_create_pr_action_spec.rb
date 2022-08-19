fd923fb6162ec84efd273b410f64373dd0ef34c7
Cesar de la Vega <cesarvegaro@gmail.com>
2022-08-19 14:19:07 +0200

    fix tests

--- spec/actions/bump_version_update_changelog_create_pr_action_spec.rb
+++ spec/actions/bump_version_update_changelog_create_pr_action_spec.rb
@@ -87,6 +87,8 @@ describe Fastlane::Actions::BumpVersionUpdateChangelogCreatePrAction do
       allow(Fastlane::Helper::VersioningHelper).to receive(:auto_generate_changelog)
         .with(mock_repo_name, mock_github_token, 3)
         .and_return(auto_generated_changelog)
+      allow(Fastlane::Helper::RevenuecatInternalHelper).to receive(:write_changelog)
+        .with(auto_generated_changelog, mock_changelog_latest_path)
       allow(Fastlane::Helper::RevenuecatInternalHelper).to receive(:create_new_branch_and_checkout)
         .with(new_branch_name)
       allow(Fastlane::Helper::RevenuecatInternalHelper).to receive(:replace_version_number)
@@ -133,6 +135,8 @@ describe Fastlane::Actions::BumpVersionUpdateChangelogCreatePrAction do
       allow(Fastlane::Helper::RevenuecatInternalHelper).to receive(:create_pr_to_main)
         .with("Release/1.13.0", edited_changelog, mock_repo_name, new_branch_name, mock_github_pr_token, labels)
 
+      expect(Fastlane::Helper::RevenuecatInternalHelper).to receive(:write_changelog)
+        .with(auto_generated_changelog, mock_changelog_latest_path)
       expect(Fastlane::Helper::RevenuecatInternalHelper).to_not(receive(:edit_changelog)
                                                                   .with(auto_generated_changelog, mock_changelog_latest_path, editor))
       Fastlane::Actions::BumpVersionUpdateChangelogCreatePrAction.run(
