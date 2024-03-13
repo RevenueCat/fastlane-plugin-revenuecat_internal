15a13d1e14be07970686b656a45ce4afc3e5d802
Cesar de la Vega <cesarvegaro@gmail.com>
2024-03-13 15:51:27 +0100

    add trigger_action

---
+++ lib/fastlane/plugin/revenuecat_internal/actions/trigger_action.rb
@@ -0,0 +1,72 @@
+require 'fastlane/action'
+require 'fastlane_core/configuration/config_item'
+require 'fastlane_core/ui/ui'
+require 'rest-client'
+require 'json'
+
+module Fastlane
+  module Actions
+    class TriggerCircleCIActionAction < Action
+      def self.run(params)
+        circle_token = params[:circle_token]
+        UI.user_error!("Please set the CIRCLE_TOKEN environment variable") unless circle_token
+
+        branch = params[:branch]
+        default_branch = Actions.git_branch
+        branch = UI.input("Branch (defaults to #{default_branch}): ")
+        branch = default_branch if branch.empty?
+
+        headers = {"Circle-Token" => circle_token, "Content-Type" => "application/json", "Accept" => "application/json"}
+        data = {parameters: {'action' => params[:action]}, branch: branch}
+        url = "https://circleci.com/api/v2/project/github/RevenueCat/#{params[:repo_name]}/pipeline"
+
+        resp = RestClient.post url, data.to_json, headers
+
+        number = JSON.parse(resp.body)["number"]
+        workflow_url = "https://app.circleci.com/pipelines/github/RevenueCat/#{params[:repo_name]}/#{number}"
+
+        UI.important "Workflow: #{workflow_url}"
+      end
+
+      def self.description
+        "Triggers a CircleCI pipeline passing an action as a parameter"
+      end
+
+      def self.authors
+        ["Cesar de la Vega"]
+      end
+
+      def self.return_value
+        "The URL of the triggered CircleCI workflow"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :circle_token,
+                                       env_name: "CIRCLE_TOKEN",
+                                       description: "The CircleCI auth token",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :action,
+                                       description: "The action to trigger",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :repo_name,
+                                       env_name: "RC_INTERNAL_REPO_NAME",
+                                       description: "Name of the repo of the SDK",
+                                       optional: false,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :branch,
+                                       description: "Branch to trigger the pipeline on, defaults to current branch",
+                                       optional: true,
+                                       type: String),
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
+
