3cba5e6c5eb2fa00d87ac03f11153de893ae90c9
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-05-06 17:27:56 +0200

    Add spaces to action options descriptions

--- lib/fastlane/plugin/revenuecat_internal/actions/create_next_snapshot_version_action.rb
+++ lib/fastlane/plugin/revenuecat_internal/actions/create_next_snapshot_version_action.rb
@@ -62,18 +62,18 @@ module Fastlane
                                        type: String),
           FastlaneCore::ConfigItem.new(key: :files_to_update,
                                        env_name: "RC_INTERNAL_FILES_TO_UPDATE_VERSION",
-                                       description: 'Hash of files that contain the version number and need to have it' \
-                                                    'updated to the patterns that contains the version in the file.' \
-                                                    'Mark the version in the pattern using {x}.' \
+                                       description: 'Hash of files that contain the version number and need to have it ' \
+                                                    'updated to the patterns that contains the version in the file. ' \
+                                                    'Mark the version in the pattern using {x}. ' \
                                                     'For example: { "./pubspec.yaml" => ["version: {x}"] }',
                                        optional: false,
                                        type: Hash),
           FastlaneCore::ConfigItem.new(key: :files_to_update_without_prerelease_modifiers,
                                        env_name: "RC_INTERNAL_FILES_TO_UPDATE_VERSION_WITHOUT_PRERELEASE_MODIFIERS",
-                                       description: 'Hash of files that contain the version number without pre-release' \
-                                                    'modifier and need to have it updated, to the patterns that' \
-                                                    'contains the version in the file.' \
-                                                    'Mark the version in the pattern using {x}.' \
+                                       description: 'Hash of files that contain the version number without pre-release ' \
+                                                    'modifier and need to have it updated, to the patterns that ' \
+                                                    'contains the version in the file. ' \
+                                                    'Mark the version in the pattern using {x}. ' \
                                                     'For example: { "./pubspec.yaml" => ["version: {x}"] }',
                                        optional: true,
                                        default_value: {},
