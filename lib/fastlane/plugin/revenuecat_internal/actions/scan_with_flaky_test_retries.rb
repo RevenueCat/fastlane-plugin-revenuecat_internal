d3c3fe2e579fcc023a6667a1f40a99f46a94cc3f
Josh Holtz <me@joshholtz.com>
2024-08-18 14:27:13 -0500

    Lint and updated description

--- lib/fastlane/plugin/revenuecat_internal/actions/scan_with_flaky_test_retries.rb
+++ lib/fastlane/plugin/revenuecat_internal/actions/scan_with_flaky_test_retries.rb
@@ -1,17 +1,20 @@
 require 'nokogiri'
 require 'scan'
+require 'fastlane_core/ui/ui'
 
 module Fastlane
+  UI = FastlaneCore::UI unless Fastlane.const_defined?(:UI)
+
   module Actions
     class ScanWithFlakyTestRetriesAction < Action
       def self.run(params)
-        # Pulling out the values from Fastlane::Configurations to 
+        # Pulling out the values from Fastlane::Configurations to
         # modify and pass into `scan`
         param_values = params.values
 
         # Delete option that isn't part of scan
         number_of_flaky_retries = param_values.delete(:number_of_flaky_retries)
-        
+
         if number_of_flaky_retries == 0
           # Run scan directly if no retries are needed
           other_action.scan(**param_values)
@@ -54,17 +57,17 @@ module Fastlane
       def self.run_test_and_retries_if_needed(params:, output_dir:, number_of_flaky_retries:)
         artifacts_dir = File.join(output_dir, 'original')
         failed_tests_path = File.join(output_dir, 'failed_tests.txt')
-      
+
         last_attempt = 0
         failed_tests = nil
         (0..number_of_flaky_retries).each do |attempt|
           last_attempt = attempt
           UI.message("Scan retry attempt #{attempt} out of #{number_of_flaky_retries}") if attempt > 0
-      
+
           # Separate report dir for each retry
           report_dir = attempt == 0 ? artifacts_dir : File.join(output_dir, "retry_#{attempt}")
           report_path = File.join(report_dir, "report.junit")
-      
+
           # Run tests
           fail_build = attempt == number_of_flaky_retries
           begin
@@ -75,7 +78,6 @@ module Fastlane
 
             other_action.scan(**params_copy)
           ensure
-
             if attempt == 0
               # Only copy original junit report and save list of failed tests
               move_junit_and_save_failed_tests(
@@ -93,7 +95,7 @@ module Fastlane
               )
             end
           end
-      
+
           # Break out of retry loop if no tests failed
           failed_tests = File.read(failed_tests_path).split("\n")
           if failed_tests.empty?
@@ -111,8 +113,8 @@ module Fastlane
       # @param source_path [String] The path of the junit file
       # @param destination_path [String] The path where the junit file should be moved to
       # @param failed_tests_path [String] The path where failed tests should be saved
-      def self.move_junit_and_save_failed_tests(source_path:, destination_path:, merge_path: nil, failed_tests_path:)
-        # Copy to junit report to 
+      def self.move_junit_and_save_failed_tests(source_path:, destination_path:, failed_tests_path:, merge_path: nil)
+        # Copy to junit report to
         FileUtils.cp(source_path, destination_path)
 
         save_failed_tests(report_path: source_path, failed_tests_path: failed_tests_path)
@@ -130,65 +132,67 @@ module Fastlane
       def self.merge_and_replace_junit(retry_report_path, original_report)
         merged_report = merge_reports(original_report, retry_report_path)
         save_report(merged_report, original_report)
-    
-        UI.message "Merged report saved to #{original_report}"
+
+        UI.message("Merged report saved to #{original_report}")
       end
 
       def self.parse_report(file_path)
         Nokogiri::XML(File.open(file_path))
       end
-      
+
+      # rubocop:disable Metrics/PerceivedComplexity
       def self.merge_reports(original_report, retry_report)
         original_doc = parse_report(original_report)
         retry_doc = parse_report(retry_report)
-      
+
         original_testcases = original_doc.xpath('//testcase')
         retry_testcases = retry_doc.xpath('//testcase')
-      
+
         retry_count = 0
-      
+
         retry_testcases.each do |retry_testcase|
           suitename = retry_testcase.parent['name'] # Retrieve suitename
           classname = retry_testcase['classname']
           name = retry_testcase['name']
-      
+
           original_testcase = original_testcases.find do |tc|
             tc['classname'] == classname && tc['name'] == name && tc.parent['name'] == suitename
           end
-      
-          if original_testcase
-            original_testcase.at('failure')&.remove # Remove failure from original
 
-            retry_failure = retry_testcase.at('failure')
+          next unless original_testcase
 
-            if retry_failure
-              # Clone the failure node from retry_testcase and add it to original_testcase
-              original_testcase.add_child(retry_failure.clone)
-            end
-      
-            # Add retry count attribute to the testcase
-            current_retry_count = original_testcase['retry_count'].to_i
-            original_testcase['retry_count'] = (current_retry_count + 1).to_s
-      
-            retry_count += 1
+          original_testcase.at('failure')&.remove # Remove failure from original
+
+          retry_failure = retry_testcase.at('failure')
+
+          if retry_failure
+            # Clone the failure node from retry_testcase and add it to original_testcase
+            original_testcase.add_child(retry_failure.clone)
           end
+
+          # Add retry count attribute to the testcase
+          current_retry_count = original_testcase['retry_count'].to_i
+          original_testcase['retry_count'] = (current_retry_count + 1).to_s
+
+          retry_count += 1
         end
-      
+
         # Add total retry count as a property in the testsuite
         properties_node = original_doc.at('testsuite > properties') || Nokogiri::XML::Node.new('properties', original_doc.at('testsuite'))
         original_doc.at('testsuite').add_child(properties_node) unless original_doc.at('testsuite > properties')
-      
+
         retry_property = Nokogiri::XML::Node.new('property', original_doc)
         retry_property['name'] = 'total_retries'
         retry_property['value'] = retry_count.to_s
-      
+
         properties_node.add_child(retry_property)
-      
+
         original_doc
       end
-      
+      # rubocop:enable Metrics/PerceivedComplexity
+
       def self.save_report(doc, output_path)
-        File.open(output_path, 'w') { |file| file.write(doc.to_xml) }
+        File.write(output_path, doc.to_xml)
       end
 
       # Saves all the failed method signatures into a text field from a junit report
@@ -197,19 +201,19 @@ module Fastlane
       # @param failed_tests_path [String] The path where failed tests should be saved
       def self.save_failed_tests(report_path:, failed_tests_path:)
         failed_tests = []
-        
+
         if File.exist?(report_path)
           doc = Nokogiri::XML(File.open(report_path))
-      
+
           doc.xpath('//testcase[failure]').each do |test_case|
             suitename = test_case.parent['name'] # Retrieve the suitename
             classname = test_case['classname']
             name = test_case['name']
             failed_tests << "#{suitename}/#{classname}/#{name}"
           end
-    
+
           failed_tests = failed_tests.uniq
-      
+
           File.open(failed_tests_path, 'w') do |file|
             file.puts(failed_tests)
           end
@@ -221,11 +225,11 @@ module Fastlane
       #####################################################
 
       def self.description
-        'A short description with <= 80 characters of what this action does'
+        'Retry flaky tests in a new scan run'
       end
 
       def self.details
-        'You can use this action to do cool things...'
+        'Retry flaky tests in a new scan run'
       end
 
       def self.available_options
@@ -234,7 +238,7 @@ module Fastlane
                                        description: 'Number of flaky retries',
                                        type: Integer,
                                        optional: false)
-        ] + FastlaneCore::CommanderGenerator.new.generate(Scan::Options.available_options)
+        ] + Scan::Options.available_options
       end
 
       def self.authors
