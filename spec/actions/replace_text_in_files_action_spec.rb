76a3a08a26bc0644f6dc444f911f9076c53825b2
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-12-09 16:52:58 +0100

    Add `skip_missing_files` parameter to `ReplaceTextInFilesAction` (#109)
    * Allow skipping missing files in ReplaceTextInFilesAction` via new parameter (that defaults to false)
    
    * Fix tests

--- spec/actions/replace_text_in_files_action_spec.rb
+++ spec/actions/replace_text_in_files_action_spec.rb
@@ -30,7 +30,65 @@ describe Fastlane::Actions::ReplaceTextInFilesAction do
 
   describe '#available_options' do
     it 'has correct number of options' do
-      expect(Fastlane::Actions::ReplaceTextInFilesAction.available_options.size).to eq(4)
+      expect(Fastlane::Actions::ReplaceTextInFilesAction.available_options.size).to eq(5)
+    end
+  end
+
+  describe '#run with skip_missing_files' do
+    it 'skips missing files when skip_missing_files is true' do
+      expect(File).to receive(:exist?).with('./missing_file.sh').and_return(false)
+      expect(Fastlane::UI).to receive(:message).with('Skipping ./missing_file.sh - file does not exist')
+      expect(Fastlane::Helper::RevenuecatInternalHelper).not_to receive(:replace_in)
+      Fastlane::Actions::ReplaceTextInFilesAction.run(
+        previous_text: 'old_text',
+        new_text: 'new_text',
+        paths_of_files_to_update: ['./missing_file.sh'],
+        skip_missing_files: true
+      )
+    end
+
+    it 'processes existing files when skip_missing_files is true' do
+      expect(File).to receive(:exist?).with('./existing_file.sh').and_return(true)
+      expect(Fastlane::Helper::RevenuecatInternalHelper).to receive(:replace_in)
+        .with('old_text', 'new_text', './existing_file.sh', allow_empty: false)
+        .once
+      Fastlane::Actions::ReplaceTextInFilesAction.run(
+        previous_text: 'old_text',
+        new_text: 'new_text',
+        paths_of_files_to_update: ['./existing_file.sh'],
+        skip_missing_files: true,
+        allow_empty: false
+      )
+    end
+
+    it 'skips missing files but processes existing ones when skip_missing_files is true' do
+      expect(File).to receive(:exist?).with('./missing_file.sh').and_return(false)
+      expect(File).to receive(:exist?).with('./existing_file.sh').and_return(true)
+      expect(Fastlane::UI).to receive(:message).with('Skipping ./missing_file.sh - file does not exist')
+      expect(Fastlane::Helper::RevenuecatInternalHelper).to receive(:replace_in)
+        .with('old_text', 'new_text', './existing_file.sh', allow_empty: false)
+        .once
+      Fastlane::Actions::ReplaceTextInFilesAction.run(
+        previous_text: 'old_text',
+        new_text: 'new_text',
+        paths_of_files_to_update: ['./missing_file.sh', './existing_file.sh'],
+        skip_missing_files: true,
+        allow_empty: false
+      )
+    end
+
+    it 'does not check file existence when skip_missing_files is false' do
+      expect(File).not_to receive(:exist?)
+      expect(Fastlane::Helper::RevenuecatInternalHelper).to receive(:replace_in)
+        .with('old_text', 'new_text', './test_file.sh', allow_empty: false)
+        .once
+      Fastlane::Actions::ReplaceTextInFilesAction.run(
+        previous_text: 'old_text',
+        new_text: 'new_text',
+        paths_of_files_to_update: ['./test_file.sh'],
+        skip_missing_files: false,
+        allow_empty: false
+      )
     end
   end
 end
