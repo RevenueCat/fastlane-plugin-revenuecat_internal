739c1f51f0039db04f2f810aa91dec12e35c673f
Toni Rico <antonio.rico.diez@revenuecat.com>
2025-12-15 15:18:19 +0100

    Revert "Add OTP warning to changelogs (#81)"
    This reverts commit 489faef4846f2a2fa8fc13f79f94631c6dda95a1.

--- lib/fastlane/plugin/revenuecat_internal/constants.rb
+++ lib/fastlane/plugin/revenuecat_internal/constants.rb
@@ -7,19 +7,3 @@ PATTERN_BUILD_METADATA = "[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*"
 PATTERN_BUILD_METADATA_ANCHORED = /^#{PATTERN_BUILD_METADATA}$/.freeze
 DELIMITER_PRERELEASE = '-'
 DELIMITER_BUILD_METADATA = '+'
-
-SDK_WARNING_VERSIONS = {
-  'purchases-unity' => '8.0.0',
-  'purchases-android' => '9.0.0',
-  'purchases-flutter' => '9.0.0',
-  'react-native-purchases' => '9.0.0',
-  'purchases-capacitor' => '11.0.0',
-  'cordova-plugin-purchases' => '7.0.0',
-  'purchases-kmp' => '2.0.0'
-}.freeze
-
-OTP_WARNING_TEXT = <<~WARNING
-  > [!WARNING]#{'  '}
-  > If you don't have any login system in your app, please make sure your one-time purchase products have been correctly configured in the RevenueCat dashboard as either consumable or non-consumable. If they're incorrectly configured as consumables, RevenueCat will consume these purchases. This means that users won't be able to restore them from version %<version>s onward.
-  > Non-consumables are products that are meant to be bought only once, for example, lifetime subscriptions.
-WARNING
