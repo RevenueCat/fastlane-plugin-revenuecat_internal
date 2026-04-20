a256c3666f11ab4b8bc550b7870d2c4df774b641
Toni Rico <antonio.rico.diez@revenuecat.com>
2026-04-20 13:56:51 +0200

    Add tests

--- spec/helper/update_hybrids_versions_file_helper_spec.rb
+++ spec/helper/update_hybrids_versions_file_helper_spec.rb
@@ -34,4 +34,50 @@ describe Fastlane::Helper::UpdateHybridsVersionsFileHelper do
       expect(version).to eq('4.9.0')
     end
   end
+
+  describe '.get_js_version_for_hybrid_common_version' do
+    let(:get_contents_js_hybrid_mappings_response) do
+      { json: JSON.parse(File.read("#{File.dirname(__FILE__)}/../test_files/get_contents_phc_js_hybrid_mappings.json")) }
+    end
+
+    def build_contents_response(body_hash)
+      { json: { 'content' => Base64.encode64(body_hash.to_json) } }
+    end
+
+    it 'obtains correct purchases-js version from github' do
+      expect(Fastlane::Actions::GithubApiAction).to receive(:run).with(
+        server_url: "https://api.github.com",
+        http_method: 'GET',
+        path: "/repos/revenuecat/purchases-hybrid-common/contents/purchases-js-hybrid-mappings/package.json?ref=18.0.0",
+        body: {},
+        api_token: 'mock-github-token'
+      ).and_return(get_contents_js_hybrid_mappings_response).once
+      version = Fastlane::Helper::UpdateHybridsVersionsFileHelper.get_js_version_for_hybrid_common_version('18.0.0', 'mock-github-token')
+      expect(version).to eq('1.34.0')
+    end
+
+    it 'raises a user error when the purchases-js dependency is missing' do
+      allow(Fastlane::Actions::GithubApiAction).to receive(:run)
+        .and_return(build_contents_response({ 'name' => '@revenuecat/purchases-js-hybrid-mappings', 'dependencies' => {} }))
+      expect do
+        Fastlane::Helper::UpdateHybridsVersionsFileHelper.get_js_version_for_hybrid_common_version('18.0.0', 'mock-github-token')
+      end.to raise_exception(FastlaneCore::Interface::FastlaneError, /Could not find purchases-js version/)
+    end
+
+    it 'raises a user error when the purchases-js dependency version is empty' do
+      allow(Fastlane::Actions::GithubApiAction).to receive(:run)
+        .and_return(build_contents_response({ 'dependencies' => { '@revenuecat/purchases-js' => '' } }))
+      expect do
+        Fastlane::Helper::UpdateHybridsVersionsFileHelper.get_js_version_for_hybrid_common_version('18.0.0', 'mock-github-token')
+      end.to raise_exception(FastlaneCore::Interface::FastlaneError, /Could not find purchases-js version/)
+    end
+
+    it 'raises when the package.json has no dependencies key at all' do
+      allow(Fastlane::Actions::GithubApiAction).to receive(:run)
+        .and_return(build_contents_response({ 'name' => '@revenuecat/purchases-js-hybrid-mappings' }))
+      expect do
+        Fastlane::Helper::UpdateHybridsVersionsFileHelper.get_js_version_for_hybrid_common_version('18.0.0', 'mock-github-token')
+      end.to raise_exception(FastlaneCore::Interface::FastlaneError, /Could not find purchases-js version/)
+    end
+  end
 end
